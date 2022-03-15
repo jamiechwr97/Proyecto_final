0a43281999faee73e69eb1317abf80f286e04fec
jamiechwr <jamiechwr97@gmail.com>
2022-03-15 13:34:55 +0100

    V1.01: Añadido estructura del proyecto + primera parte de la página home hecha

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './features/pages/home/home.component';
+import { CategoriasComponent } from './features/pages/categorias/categorias.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch:'full', redirectTo:'home' },
+  { path: 'home', component: HomeComponent },
+  /*{ path: 'categorias', component: CategoriasComponent },
+  { path: 'categorias/:nombre' },
+  { path: 'concierto/:nombre' },
+  { path: 'artistas' },
+  { path: 'artistas/:nombre' },
+  { path: 'register' },*/
+  { path: '**', component: HomeComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './features/pages/home/home.component';
+import { CategoriasComponent } from './features/pages/categorias/categorias.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    HomeComponent,
+    CategoriasComponent
   ],
   imports: [
     BrowserModule,
