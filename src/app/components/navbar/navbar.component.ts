a7282eda86752971372f6dd881c8fbde5f20c317
jamiechwr <jamiechwr97@gmail.com>
2022-03-22 15:42:45 +0100

    fix: arreglado que las categorias se refresquen al cambiar de categoria desde esa misma página

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
+import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +9,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  route: string;
+
+  constructor(location: Location, private router: Router) {
+    router.events.subscribe(val => {
+      if (location.path() != "") {
+        this.route = location.path();
+        console.log(this.route);
+      } else {
+        this.route = "Home";
+      }
+    });
+
+    this.refresh();
+  }
 
   ngOnInit(): void {
+  }
+
+  refresh() {
+    let currentUrl = this.router.url;
+    this.router.routeReuseStrategy.shouldReuseRoute = () => false;
+    this.router.onSameUrlNavigation = 'reload';
+    this.router.navigate([currentUrl]);
   }
 
 }
